8384eb5a16c07aefff7cf3479013fc15b9dd72b5
c-wegner <cwegner@wegnerlawfirm.com>
2021-04-02 10:21:31 -0400

    What

---
+++ journal-4-2-21/src/App.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { GlobalStyle } from "./globals/styles";
+
+
+function App() {
+  return (
+    <div>
+      <GlobalStyle />
+      Testing
+    </div>
+  );
+}
+
+export default App;
---
+++ journal-4-2-21/src/globals/styles.tsx
@@ -0,0 +1,7 @@
+import styled, { createGlobalStyle } from 'styled-components';
+
+export const GlobalStyle = createGlobalStyle`
+    :root{
+        font-family: Segoe UI,Frutiger,Frutiger Linotype,Dejavu Sans,Helvetica Neue,Arial,sans-serif;  
+    }
+`;
