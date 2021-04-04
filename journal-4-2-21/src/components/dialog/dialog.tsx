726470665810de2cf7aa6bc72336dc3af3724eaa
c-wegner <cwegner@wegnerlawfirm.com>
2021-04-04 17:00:20 -0400

    dddd

--- journal-4-2-21/src/components/dialog/dialog.tsx
+++ journal-4-2-21/src/components/dialog/dialog.tsx
@@ -3,11 +3,20 @@ import styled from 'styled-components';
 import { common } from "../../globals";
 
 const Styles = {
-  Container: styled.div `
+  Container: styled.div<{display:string}> `
     margin: 0 auto;
     margin-top: 20%;
     width: 100%;
     max-width: 700px;
-    
+    box-shadow: ${common.values.shadow.hover};
+    display: ${p=>p.display};
   `
+}
+
+export const Dialog=({children, onExit})=>{
+  return(
+    <Styles.Container display={'flex'}>
+      {children}
+    </Styles.Container>
+  )
 }
