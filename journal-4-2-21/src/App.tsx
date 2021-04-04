36997b6145006926fc5b8ac1126abe0324c6b803
c-wegner <cwegner@wegnerlawfirm.com>
2021-04-04 17:08:49 -0400

    eee

--- journal-4-2-21/src/App.tsx
+++ journal-4-2-21/src/App.tsx
@@ -1,23 +1,31 @@
 import React from 'react';
 import { useContext } from 'react';
+import { useState } from 'react';
 import { Fragment } from 'react';
+import { Dialog } from './components/dialog/dialog';
 import { GlobalStyle } from "./globals/styles";
 import { ClientsContext, ClientsProvider, ProjectsProvider, TimeProvider } from './models';
 
 
 
 function App() {
+  const [dTest, setDTest] = useState('')
   return (
     <Fragment>
       <ClientsProvider>
         <ProjectsProvider>
           <TimeProvider>
             <GlobalStyle />
+            <span onClick={()=>setDTest('Test')}>
       Testing
+      </span>
       <TestDB/>
       </TimeProvider>
         </ProjectsProvider>
       </ClientsProvider>
+      <Dialog id='Test' onExit={()=>setDTest('')} current={dTest}>
+        Tada
+      </Dialog>
     </Fragment>
   );
 }
--- journal-4-2-21/src/components/dialog/dialog.tsx
+++ journal-4-2-21/src/components/dialog/dialog.tsx
@@ -10,12 +10,39 @@ const Styles = {
     max-width: 700px;
     box-shadow: ${common.values.shadow.hover};
     display: ${p=>p.display};
-  `
+    flex-direction: column;
+    border-radius: ${common.values.borderRadius};
+    border: ${common.values.border};
+    padding: ${common.values.padding};
+  `,
+
+  ExitBar: styled.div `
+    display: flex;
+    justify-content: flex-end;
+  `,
+
+  ExitMark: styled.div `
+  cursor: pointer;
+    &:hover{
+      color: red;
+    }
+  `,
 }
 
-export const Dialog=({children, onExit})=>{
+export const Dialog=({id, current, children, onExit})=>{
+
+  const display=()=>{
+    const temp = id === current
+    if(temp) return 'flex'
+    return 'none'
+  }
   return(
-    <Styles.Container display={'flex'}>
+    <Styles.Container display={display()}>
+      <Styles.ExitBar>
+        <Styles.ExitMark onClick ={()=>onExit()}>
+          x
+        </Styles.ExitMark>
+      </Styles.ExitBar>
       {children}
     </Styles.Container>
   )
