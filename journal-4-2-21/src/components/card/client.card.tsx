561617df34b81cb0dc758328a5ead2fac2353df7
c-wegner <cwegner@wegnerlawfirm.com>
2021-04-09 12:42:47 -0400

    dfdd

--- journal-4-2-21/src/components/card/client.card.tsx
+++ journal-4-2-21/src/components/card/client.card.tsx
@@ -1,3 +1,22 @@
 import React, {useState} from 'react';
+import { useEffect } from 'react';
 import styled from 'styled-components';
+import { Client } from '../../models';
 
+export const ClientCard =({
+  client = new Client(),
+  currentClient = new Client(),
+  onSelectClient
+})=>{
+  const [expanded, setExpanded] = useState(false);
+
+  useEffect(){
+
+
+  }
+
+  const handleClick=()=>{
+    setExpanded(!expanded);
+    onSelectClient(client)
+  }
+}
