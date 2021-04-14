fddcbde1bfa97cca6c57a3359598ba983b9e8a5a
c-wegner <cwegner@wegnerlawfirm.com>
2021-04-14 13:53:13 -0400

    sss

--- journal-4-2-21/src/components/billing/billing.main.tsx
+++ journal-4-2-21/src/components/billing/billing.main.tsx
@@ -27,11 +27,26 @@ const BillItemStyle = styled.div<{width: string, justifyContent?:string, flexGro
   background-color: inherit;
 `
 
+const HeaderStyle = styled.div`
+display: flex;
+flex-direction: row;
+width: 100%;
+flex-grow: 1;
+background-color: white;
+padding: 7px;
+
+font-weight: 500;
+
+border-bottom: 1px solid;
+padding: 0 0 3px 0;
+margin-bottom: 5px;
+`
+
 export const BillingSheet = ({})=>{
   const timeSheet = useContext(TimeContext)
   return(
     <BillPageStyle>
-
+      <Headers/>
   
       {
         timeSheet._times.map((x,i)=>{
@@ -68,6 +83,36 @@ const BillingLine = ({time = new Time(), currentLine = 0})=>{
       <BillItemStyle width='5%' justifyContent='flex-end'>
         {time.time}
       </BillItemStyle>
+      <BillItemStyle width='10%' justifyContent='flex-end'>
+        {time.rate}
+      </BillItemStyle>
+      <BillItemStyle width='10%' justifyContent='flex-end'>
+        {time.rate * time.time}
+      </BillItemStyle>
     </BillLineStyle>
   )
 }
+
+const Headers=()=>(
+  <HeaderStyle>
+      
+  <BillItemStyle width='10%'>
+    Date
+  </BillItemStyle>
+  <BillItemStyle width='15%'>
+    Client
+  </BillItemStyle>
+  <BillItemStyle width='30%^' flexGrow='1'>
+    Description
+  </BillItemStyle>
+  <BillItemStyle width='5%' justifyContent='flex-end'>
+    Time
+  </BillItemStyle>
+  <BillItemStyle width='10%'  justifyContent='flex-end'>
+    Rate
+  </BillItemStyle>
+  <BillItemStyle width='10%'  justifyContent='flex-end'>
+    Total
+  </BillItemStyle>
+</HeaderStyle>
+)
