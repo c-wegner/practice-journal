5db015a89791e7d05b80654b7c80bfa2234f13f0
c-wegner <cwegner@wegnerlawfirm.com>
2021-05-03 15:01:15 -0400

    eeeeeee

--- journal-4-2-21/src/_models/clients/client._model.context.tsx
+++ journal-4-2-21/src/_models/clients/client._model.context.tsx
@@ -31,6 +31,8 @@ export const ClientsProvider = ({ children }) => {
 
           }
         }
+
+        c.phone= formatPhoneNumber(c.phone)
         book.addClient(c)
       });
       setClientsData(book.clients);
@@ -59,4 +61,28 @@ function cloneClient(obj: any): Client {
 function getSecondsFromLastTick(lastTick: number){
   let delta = new Date().getTime() - lastTick
   return delta/1000
+}
+
+function formatPhoneNumber(pho){
+	const l = pho.length;
+	let temp = '';	
+	for(let i =0; i<l; i++){
+		const c = pho[i]
+		if(!isNaN(c)){
+			temp = temp + c
+		}
+	}
+	
+	const tl = temp.length;
+	if(tl === 7){
+		return temp.substring(0,3) + '-' + temp.substring(3)
+	}
+	if(tl === 10){
+		return '('+temp.substring(0,3)+') ' + temp.substring(3,6) +'-' + temp.substring(6)
+	}
+	
+	if(tl === 11){
+		return '+'+ temp.substring(0,1) + ' ('+temp.substring(1,4)+')' + temp.substring(4,7) +'-' + temp.substring(7)
+	}
+	return pho
 }
