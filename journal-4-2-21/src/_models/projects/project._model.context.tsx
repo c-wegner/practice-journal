06ee32859e2862c897003ea8e5d140eae5acfb39
c-wegner <cwegner@wegnerlawfirm.com>
2021-05-08 15:47:49 -0400

    sssssssssss

--- journal-4-2-21/src/_models/projects/project._model.context.tsx
+++ journal-4-2-21/src/_models/projects/project._model.context.tsx
@@ -10,24 +10,17 @@ export const ProjectsProvider = ({ children }) => {
   const [projects, setProjects] = useState([])
   const list = new Projects()
 
-  let wegnerStoredTimerData = null;
-
   const loadProjects = () => {
     const db = firebase.firestore(app);
-    if (localStorage.getItem('wegnerStoredTimerData')) {
-      wegnerStoredTimerData = JSON.parse(localStorage.getItem('wegnerStoredTimerData'))
-    }
+
     db.collection(projectPath).onSnapshot(function (querySnapshot) {
       list.projects = []
       querySnapshot.forEach(function (doc) {
         const p = cloneProject(doc.data());
         p.hasOpenTime = false;
         p.timerIsRunning = true;
-        if (localStorage.getItem('wegnerStoredTimerData')) {
-          if(wegnerStoredTimerData.objId===p.id && wegnerStoredTimerData.objType==='project'){
-            p.hasOpenTime = true
-           p.timerIsRunning = wegnerStoredTimerData.timerRunning
-          }
+        if(p.assignedTo===undefined || p.assignedTo===''){
+          p.assignedTo = 'cwegner'
         }
         list.addProject(p)
       });
